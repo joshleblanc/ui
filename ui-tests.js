a4227e5606d0348b3b604eeddbac32833a06ef31
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-04-02 20:54:31 -0300

    export crayon

--- ui-tests.js
+++ ui-tests.js
@@ -2,7 +2,7 @@
 import { Tinytest } from "meteor/tinytest";
 
 // Import and rename a variable exported by ui.js.
-import { name as packageName } from "meteor/ui";
+import { name as packageName } from "meteor/cereal:ui";
 
 // Write your tests here!
 // Here is an example.
