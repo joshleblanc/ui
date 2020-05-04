f2785917660b3257aca16a81b2e5013a6ffc48f2
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-05-04 10:51:40 -0300

    add innerlayout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
-import {Content} from "./base/Content";
-import Navbar from "./Navbar";
-import Nav from "./Nav";
-import {Providers} from "./Providers";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import {Root} from "./base/Root";
-import { Switch } from 'react-router-dom';
 import {Boilerplate} from "./Boilerplate";
+import {InnerLayout} from "./InnerLayout";
 
 export default props => {
   const {basename, Routes, DrawerItems} = props;
   return (
     <Boilerplate basename={basename}>
-      <Navbar/>
-      <Nav><DrawerItems/></Nav>
-      <Content>
-        <Routes/>
-      </Content>
+      <InnerLayout Routes={Routes} DrawerItems={DrawerItems} />
     </Boilerplate>
-
-)
+  )
 };
