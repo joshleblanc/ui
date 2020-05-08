3a28e96302e0c3c93448f3c7ae064b16a4f47179
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-05-08 18:09:47 -0300

    remove npm deps

--- package.js
+++ package.js
@@ -20,12 +20,6 @@ Package.onUse(function(api) {
   api.addAssets("public/resources/background-colored.jpg", "client")
 });
 
-
-Npm.depends({
-  "@babel/core": "7.4.3",
-  "@babel/plugin-proposal-decorators": "7.8.3"
-});
-
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
