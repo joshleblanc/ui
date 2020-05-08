4c98a022880fd17ba83d7c90028da101eee77f9c
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-05-08 09:03:35 -0300

    up version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cereal:ui',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Common UI elements',
   description: "Common UI elements",
