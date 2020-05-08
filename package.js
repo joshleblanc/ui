e36baba3d33d050f5d62743af15464f3d3261b85
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-05-08 10:07:13 -0300

    bump ui

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cereal:ui',
-  version: '0.0.4',
+  version: '0.0.5',
   // Brief, one-line summary of the package.
   summary: 'Common UI elements',
   description: "Common UI elements",
